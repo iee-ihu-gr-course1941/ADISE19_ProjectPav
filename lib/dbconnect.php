d91d16f78e4759b5c0b012ca5e36be659647719a
Dimitris Pavlidis <57565997+dpavlidis@users.noreply.github.com>
2020-01-13 23:29:22 +0200

    update

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,15 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$host='localhost';
 $db = 'adise19_uno_game_db';
 
-// Create connection
-$mysqli = new mysqli($servername, $username, $password, $db);
+$user=$DB_USER;
+$pass=$DB_PASS;
 
-// Check connection
-if (mysqli_connect_error()) {
-    die("Database connection failed: " . mysqli_connect_error());   
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2014/it144242/mysql/run/mysql.sock');
+} else {
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . 
+    $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
 ?>
